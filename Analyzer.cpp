4f0718d94d7c50392409f598c7f1354d402e4fd8
Alireza Khadem <akhasdf1383@gmail.com>
2024-04-02 07:44:14 +0330

    reservedword & identifier added

--- Analyzer.cpp
+++ Analyzer.cpp
@@ -3,11 +3,9 @@
 #include <fstream>
 #include <algorithm>
 using namespace std;
-
-char letter[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
-char digit[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-string reservedword[15] = {"int", "float", "void", "return", "if", "while", "cin", "cout", "continue", "break", "#include", "using", "namespace", "std", "main"};
+string letter[52] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+string digit[10] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+string reservedword[16] = {"int", "float", "void", "return", "if", "while", "cin", "cout", "continue", "break", "#include", "using", "namespace", "std", "main", "for"};
 
 string symbol[23] = {"(", ")", "{", "}", "[", "]", ",", ";", "+", "-", "*", "/", "==", "!=", ">", ">=", "<", "<=", "=", "&&", "||", "<<", ">>"};
 
@@ -28,13 +26,53 @@ void tokenize(string input)
                 cout << "<Symbol , " << input[i] << ">" << endl;
             }
         }
+        // digit & number tokenize
+        else if (find(begin(digit), end(digit), string(1, input[i])) != end(digit))
+        {
+            string k = "";
+            while (i + 1 < input.length() && find(begin(digit), end(digit), string(1, input[i + 1])) != end(digit))
+            {
+                k = k + input[i];
+                i++;
+            }
+            k = k + input[i];
+            cout << "<Number , " << k << ">" << endl;
+        }
+        // reservedword & identifier tokenize
+        else if (find(begin(letter), end(letter), string(1, input[i])) != end(letter))
+        {
+            string word;
+            while (find(begin(letter), end(letter), string(1, input[i])) != end(letter))
+            {
+                word += input[i];
+                i++;
+            }
+            i--;
+            if (word == "include")
+            {
+                word = "#include";
+                while (input[i] != '>')
+                {
+                    i++;
+                }
+            }
+            if (find(begin(reservedword), end(reservedword), word) != end(reservedword))
+            {
+                cout << "<reservedword , " << word << ">" << endl;
+            }
+            else
+            {
+                cout << "<identifier , " << word << ">" << endl;
+            }
+        }
     }
 }
 
 int main()
 {
+    ifstream file("input.cpp");
     string input, temp;
-    while (getline(cin, temp))
+    while (getline(file, temp))
     {
         input += temp;
         if (temp == "")
--- input.cpp
+++ input.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 int main()
 {
-    for (int i = 0; i < 5; i++)
+    int x;
+    int s = 0, t = 10;
+    while (t >= 0)
     {
-        cout << i << endl;
+        cin >> x;
+        t = t - 1;
+        s = s + x;
     }
+    cout << "sum=" << s;
     return 0;
 }
